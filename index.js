cbce9424ea3ecab68080a8d9c99bd62f1e49c925
cuonghtkt <cuong01629434877@gmail.com>
2022-01-05 17:02:30 +0700

    5/1 start

---
+++ index.js
@@ -0,0 +1,21 @@
+// node js code
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on("chat", function(msg) {
+        console.log('message f  rom user#' + socket.userId + ": " + msg);
+    });
+    // io.emit('chat', "server result");
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+http.listen(3000, () => {
+    console.log('Connected at 3000');
+});
